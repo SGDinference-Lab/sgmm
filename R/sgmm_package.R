fd2d13d43749303c53a93880457f0885fd214551
Sokbae "Simon" Lee <sl3841@columbia.edu>
2023-04-24 10:29:26 -0400

    Description/Namespace updated

---
+++ R/sgmm_package.R
@@ -0,0 +1,11 @@
+#' SGMM: Estimation and Inference via Random Scaling
+#'
+#' sgmm computes the SGMM estimator and the confidence interval via random scaling method.
+#'
+#' @docType package
+#' @author Xiaohong Chen, Sokbae Lee, Yuan Liao, Myung Hwan Seo, Youngki Shin, Myunghyun Song
+#' @import Rcpp stats abind
+#' @importFrom Rcpp sourceCpp
+#' @useDynLib sgmm
+#' @name sgmm
+NULL
